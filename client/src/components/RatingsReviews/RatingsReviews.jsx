db6bd227f94dc073dadc2167df02ab93048e2d5e
wjloo95 <loo.williamj@gmail.com>
2020-02-13 19:19:31 -0500

    Implement Brotli/GZip

--- client/src/components/RatingsReviews/RatingsReviews.jsx
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -44,7 +44,7 @@ import ProductBreakdown from './ProductBreakdown/ProductBreakdown';
 //   }
 // }
 
-const RatingsReviews = (props) => {
+const RatingsReviews = props => {
   const { id } = useParams();
 
   const [url, seturl] = useState(id);
@@ -54,27 +54,32 @@ const RatingsReviews = (props) => {
   const [ratingAvg, setRatingAvg] = useState(0);
   const [sortParameter, setSortParameter] = useState('relevant');
   const [filter, setFilter] = useState(false);
-  const [filterBy, setFilterBy] = useState({ 5: false, 4: false, 3: false, 2: false, 1: false });
+  const [filterBy, setFilterBy] = useState({
+    5: false,
+    4: false,
+    3: false,
+    2: false,
+    1: false
+  });
 
   // Returns false if any of the filterBy's are true
-  const allFiltersAreFalse = (filters) => {
+  const allFiltersAreFalse = filters => {
     let allAreFalse = true;
-    
+
     for (let rating in filters) {
       if (filters[rating]) {
         allAreFalse = false;
       }
     }
-    
+
     return allAreFalse;
-  }
-  
+  };
+
   // When a rating is clicked, toggle FilterBy
-  const toggleFilterBy = (num) => {
-    
-    let tempFilters = {...filterBy, [num]: !filterBy[num]};
-    
-    setFilterBy({...filterBy, [num]: !filterBy[num]});
+  const toggleFilterBy = num => {
+    let tempFilters = { ...filterBy, [num]: !filterBy[num] };
+
+    setFilterBy({ ...filterBy, [num]: !filterBy[num] });
 
     // If any of the filterBy's are true, set the filter to false
     if (allFiltersAreFalse(tempFilters)) {
@@ -82,75 +87,93 @@ const RatingsReviews = (props) => {
     } else {
       setFilter(true);
     }
-  }
+  };
 
   // Sets all the filters to false
   const clearFilters = () => {
     setFilter(false);
     setFilterBy({ 5: false, 4: false, 3: false, 2: false, 1: false });
-  }
+  };
 
   if (url !== id) {
     seturl(id);
   }
 
   useEffect(() => {
-    axios.get(`http://3.134.102.30/reviews/${url}/meta`)
-      .then(( {data} ) => {
-        setMeta(data);
-        
-        let totalQuantity = 0;
-        let ratingSum = 0;
+    axios.get(`http://3.134.102.30/reviews/${url}/meta`).then(({ data }) => {
+      setMeta(data);
 
-        for (let rating in data.ratings) {
-          totalQuantity += data.ratings[rating];
-          ratingSum += rating * data.ratings[rating];
-        }
+      let totalQuantity = 0;
+      let ratingSum = 0;
 
-        setTotalRatings(totalQuantity);
-        
-        let ratingAvg = ratingSum / totalQuantity;
-        // setRatingAvg(ratingAvg);
+      for (let rating in data.ratings) {
+        totalQuantity += data.ratings[rating];
+        ratingSum += rating * data.ratings[rating];
+      }
 
-        props.setAppAvg(ratingAvg);
+      setTotalRatings(totalQuantity);
 
-        axios.get(`http://3.134.102.30/reviews/${url}/list?page=1&count=${totalQuantity}&sort=${sortParameter}`)
-          .then(( {data} ) => {
-            props.setAppTotal(data.results.length)
-            setReviewsList(data.results);
-          });
-      });
+      let ratingAvg = ratingSum / totalQuantity;
+      // setRatingAvg(ratingAvg);
+
+      props.setAppAvg(ratingAvg);
+
+      axios
+        .get(
+          `http://3.134.102.30/reviews/${url}/list?page=1&count=${totalQuantity}&sort=${sortParameter}`
+        )
+        .then(({ data }) => {
+          props.setAppTotal(data.results.length);
+          setReviewsList(data.results);
+        });
+    });
   }, [url]);
 
   useEffect(() => {
-    axios.get(`http://3.134.102.30/reviews/${url}/list?page=1&count=${props.totalReviews}&sort=${sortParameter}`)
-      .then(( {data} ) => {
+    axios
+      .get(
+        `http://3.134.102.30/reviews/${url}/list?page=1&count=${props.totalReviews}&sort=${sortParameter}`
+      )
+      .then(({ data }) => {
         props.setAppTotal(data.results.length);
         setReviewsList(data.results);
       });
   }, [sortParameter]);
 
-  const changeSortParameter = (parameter) => {
+  const changeSortParameter = parameter => {
     if (parameter !== undefined || parameter !== null) {
       setSortParameter(parameter);
     }
-  }
+  };
 
   return (
-    <div id="ratings-reviews" >
+    <div id="ratings-reviews">
       <div>
-        <h1 className="top-container" >
-          Ratings & Reviews
-        </h1>
+        <h1 className="top-container">Ratings & Reviews</h1>
       </div>
-      <div className="bottom-container" >
-        <div className="left-container" >
-          <RatingBreakdown recommended={meta.recommended} ratings={meta.ratings} ratingAverage={props.ratingAverage} totalRatings={totalRatings} toggleFilterBy={toggleFilterBy} clearFilters={clearFilters} />
+      <div className="bottom-container">
+        <div className="left-container">
+          <RatingBreakdown
+            recommended={meta.recommended}
+            ratings={meta.ratings}
+            ratingAverage={props.ratingAverage}
+            totalRatings={totalRatings}
+            toggleFilterBy={toggleFilterBy}
+            clearFilters={clearFilters}
+          />
           <ProductBreakdown characteristics={meta.characteristics} />
         </div>
-        <div className="right-container" >
-          <SortOptions totalReviews={props.totalReviews} changeSortParameter={changeSortParameter} />
-          <ReviewsList reviewsList={reviewsList} filter={filter} filterBy={filterBy} characteristics={meta.characteristics} />
+        <div className="right-container">
+          <SortOptions
+            totalReviews={props.totalReviews}
+            changeSortParameter={changeSortParameter}
+          />
+          <ReviewsList
+            reviewsList={reviewsList}
+            filter={filter}
+            filterBy={filterBy}
+            characteristics={meta.characteristics}
+          />
         </div>
       </div>
     </div>
